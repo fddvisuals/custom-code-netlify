4f38a900356d88d98b7adfcefb2e33c612250248
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-11-24 21:34:04 +0530

    Update Iran-monograph-range-circles-script.js

--- Iran-monograph-range-circles-script.js
+++ Iran-monograph-range-circles-script.js
@@ -213,7 +213,7 @@ map.on("load", function (e) {
       if (target.length) {
         $("html,.missile-wrapper").animate(
           {
-            scrollTop: target.offset().top + 300,
+            scrollTop: target.offset().top + 10,
           },
           0
         );
