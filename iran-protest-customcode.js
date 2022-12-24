5f58ed6da25e6edaa22ac3504a76e72df3820cf8
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-24 11:18:21 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -41,7 +41,42 @@ $.ajax(
 ).done(function (lastupdated) {
   document.getElementById("last-updated").innerHTML = lastupdated;
 });
-
+$(".ticker-text").each(function (index) {
+  // assign ID
+  let thisId = "countup" + index;
+  $(this).attr("id", thisId);
+  // create variables
+  let startNumber = +$(this).text();
+  let endNumber = +$(this).attr("final-number");
+  let decimals = 0;
+  let duration = 2.5;
+  // animate number
+  let myCounter = new CountUp(
+    thisId,
+    startNumber,
+    endNumber,
+    decimals,
+    duration
+  );
+  // Scroll out of view trigger
+  ScrollTrigger.create({
+    trigger: $(this),
+    start: "top bottom",
+    end: "bottom top",
+    onLeaveBack: () => {
+      myCounter.reset();
+    },
+  });
+  // Scroll into view trigger
+  ScrollTrigger.create({
+    trigger: $(this),
+    start: "top 80%",
+    end: "bottom top",
+    onEnter: () => {
+      myCounter.start();
+    },
+  });
+});
 $(document).ready(function () {
   $.ajax({
     type: "GET",
