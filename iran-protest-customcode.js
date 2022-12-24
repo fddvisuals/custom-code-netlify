1b9f6c0e67565647b0314d8e9f54d6fffaf25052
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-24 11:24:38 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -41,42 +41,42 @@ $.ajax(
 ).done(function (lastupdated) {
   document.getElementById("last-updated").innerHTML = lastupdated;
 });
-$(".ticker-text").each(function (index) {
-  // assign ID
-  let thisId = "countup" + index;
-  $(this).attr("id", thisId);
-  // create variables
-  // let startNumber = +$(this).text();
-  let endNumber = lastupdated;
-  let decimals = 0;
-  let duration = 2.5;
-  // animate number
-  let myCounter = new CountUp(
-    thisId,
-    // startNumber,
-    endNumber,
-    decimals,
-    duration
-  );
-  // Scroll out of view trigger
-  ScrollTrigger.create({
-    trigger: $(this),
-    start: "top bottom",
-    end: "bottom top",
-    onLeaveBack: () => {
-      myCounter.reset();
-    },
-  });
-  // Scroll into view trigger
-  ScrollTrigger.create({
-    trigger: $(this),
-    start: "top 80%",
-    end: "bottom top",
-    onEnter: () => {
-      myCounter.start();
-    },
-  });
-});
+// $(".ticker-text").each(function (index) {
+//   // assign ID
+//   let thisId = "countup" + index;
+//   $(this).attr("id", thisId);
+//   // create variables
+//   // let startNumber = +$(this).text();
+//   let endNumber = lastupdated;
+//   let decimals = 0;
+//   let duration = 2.5;
+//   // animate number
+//   let myCounter = new CountUp(
+//     thisId,
+//     // startNumber,
+//     endNumber,
+//     decimals,
+//     duration
+//   );
+//   // Scroll out of view trigger
+//   ScrollTrigger.create({
+//     trigger: $(this),
+//     start: "top bottom",
+//     end: "bottom top",
+//     onLeaveBack: () => {
+//       myCounter.reset();
+//     },
+//   });
+//   // Scroll into view trigger
+//   ScrollTrigger.create({
+//     trigger: $(this),
+//     start: "top 80%",
+//     end: "bottom top",
+//     onEnter: () => {
+//       myCounter.start();
+//     },
+//   });
+// });
 $(document).ready(function () {
   $.ajax({
     type: "GET",
