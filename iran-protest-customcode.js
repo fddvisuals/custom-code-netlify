32d82966b032cb306bc71e49502f16204b9a3639
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-29 20:11:58 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -311,7 +311,7 @@ function makecall(initobj) {
   //replace it with your values
   makecall({
     sheetid: "1k9pmYP2UBYC7N5uIkXYy5UR4B15cFr1zpNSbCOg2BHo",
-    apikey: "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",
+    apikey: "aiCnb4mdXnPPURa5D0rOab_SEWwm",
     parentdivclass: ".main-div",
     elementdivclass: ".sing-list",
     emptylist: ".empty-list",
