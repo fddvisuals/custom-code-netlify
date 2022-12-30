b88dc0b3b491b1d8742f0225f16155060bef535c
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-30 11:34:39 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -1,7 +1,41 @@
-var transformRequest = (url, resourceType) => {
-  var isMapboxRequest =
-    url.slice(8, 22) === "api.mapbox.com" ||
-    url.slice(10, 26) === "tiles.mapbox.com";
+// Detect when element are loaded
+function waitForElement(selector) {
+  return new Promise(function (resolve, reject) {
+    let element = document.querySelector(selector);
+
+    if (element) {
+      resolve(element);
+      return;
+    }
+
+    let observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        let nodes = Array.from(mutation.addedNodes);
+        for (let node of nodes) {
+          if (node.matches && node.matches(selector)) {
+            observer.disconnect();
+            resolve(node);
+            return;
+          }
+        }
+      });
+    });
+
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+// Hide loader when markers are visible
+waitForElement(".marker").then(function (element) {
+  document.querySelector(".loader").style.display = "none";
+});
+
+let transformRequest = (url, resourceType) => {
+  const isMapboxRequest = url.indexOf("mapbox.com") !== -1;
+
   return {
     url: isMapboxRequest ? url.replace("?", "?pluginName=sheetMapper&") : url,
   };
@@ -75,26 +109,7 @@ function init() {
   );
 }
 // Hide loader when markers are visible
-waitForElement(".marker").then(function (element) {
-  document.querySelector(".loader").style.display = "none";
-});
 
-let transformRequest = (url, resourceType) => {
-  const isMapboxRequest = url.indexOf("mapbox.com") !== -1;
-
-  return {
-    url: isMapboxRequest ? url.replace("?", "?pluginName=sheetMapper&") : url,
-  };
-};
-
-function mapZoom() {
-  width = document.documentElement.clientWidth;
-  if (width < 768) {
-    return 10.2;
-  } else {
-    return 12;
-  }
-}
 function buildPopup(row) {
   let popup = `<div class="popup"><div class="popup-date"><div class="icon w-embed"><svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M17 3H21C21.2652 3 21.5196 3.10536 21.7071 3.29289C21.8946 3.48043 22 3.73478 22 4V20C22 20.2652 21.8946 20.5196 21.7071 20.7071C21.5196 20.8946 21.2652 21 21 21H3C2.73478 21 2.48043 20.8946 2.29289 20.7071C2.10536 20.5196 2 20.2652 2 20V4C2 3.73478 2.10536 3.48043 2.29289 3.29289C2.48043 3.10536 2.73478 3 3 3H7V1H9V3H15V1H17V3ZM4 9V19H20V9H4ZM6 11H8V13H6V11ZM11 11H13V13H11V11ZM16 11H18V13H16V11Z" fill="currentColor"></path>
