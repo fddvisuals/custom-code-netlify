f26c190a64d23962286897a8b925cd710d637a4b
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-30 15:38:24 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -164,12 +164,7 @@ function addPoints(data) {
     let popup = new mapboxgl.Popup().setHTML(buildPopup(row));
     let Long = row.Longitude;
     let Lat = row.Latitude;
-    const itemLink = document.createElement("a");
-    itemLink.href - "#";
-    itemLink.target = "_blank";
-    itemLink.className = "link-block w-inline block";
-    itemLink.innerHTML = `<div class="title-wrapper-listing"><div class="list-blue-title"><img src="" loading="lazy" alt="" class="listing-icon red"><p class="bold-listing-title-mil"> ${row.Date} | ${row.District} <br><span class="group-name-span">${row.Province}</span></p></div><div class="subtitle-wrapper"><div class="listing-subtitle-wrapper"><img src="https://uploads-ssl.webflow.com/6352289bab9b05d2a93f26f6/6381d9eac6b1ef242239cb16_Vector-1.svg" loading="lazy" alt="" class="listing-sub-icon"><p class="bold-listing-subtitle">${row.Date}</p></div><div class="listing-subtitle-wrapper"><img src="https://uploads-ssl.webflow.com/6352289bab9b05d2a93f26f6/6381d9eac50a51f40b798700_Vector.svg" loading="lazy" alt="" class="listing-sub-icon"><p class="bold-listing-subtitle">${row.Estimated_Size}</p></div></div></div><p class="listing-text">${row.Description}</p>`;
-    listingEl.appendChild(itemLink);
+    generateListings(row);
     let el = document.createElement("div");
     el.className = "marker";
     if (row.Estimated_Size == "Large") {
@@ -202,4 +197,13 @@ function addPoints(data) {
       console.log(`Error: ${error}. Row: ${row.webid}`);
     }
   });
+
+  function generateListings(row) {
+    const itemLink = document.createElement("a");
+    itemLink.href - "#";
+    itemLink.target = "_blank";
+    itemLink.className = "link-block w-inline block";
+    itemLink.innerHTML = `<div class="title-wrapper-listing"><div class="list-blue-title"><img src="" loading="lazy" alt="" class="listing-icon red"><p class="bold-listing-title-mil"> ${row.Date} | ${row.District} <br><span class="group-name-span">${row.Province}</span></p></div><div class="subtitle-wrapper"><div class="listing-subtitle-wrapper"><img src="https://uploads-ssl.webflow.com/6352289bab9b05d2a93f26f6/6381d9eac6b1ef242239cb16_Vector-1.svg" loading="lazy" alt="" class="listing-sub-icon"><p class="bold-listing-subtitle">${row.Date}</p></div><div class="listing-subtitle-wrapper"><img src="https://uploads-ssl.webflow.com/6352289bab9b05d2a93f26f6/6381d9eac50a51f40b798700_Vector.svg" loading="lazy" alt="" class="listing-sub-icon"><p class="bold-listing-subtitle">${row.Estimated_Size}</p></div></div></div><p class="listing-text">${row.Description}</p>`;
+    listingEl.appendChild(itemLink);
+  }
 }
