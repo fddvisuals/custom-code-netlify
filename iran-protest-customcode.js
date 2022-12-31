5719c275db480e1905b7bd5ad04a00c7baeeb34b
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-31 17:21:23 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -142,7 +142,7 @@ map.on("load", function () {
             $(".popup-grid-div-killed").toggleClass("is-hidden");
           }
         });
-        map.on("mouseleave", "csvData", function (e) {
+        map.on("mouseleave", "csvData", function () {
           map.getCanvas().style.cursor = "";
           popup.remove();
         });
