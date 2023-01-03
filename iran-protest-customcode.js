231bba3cd1d557539c9c498362bd7d26ed129024
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2023-01-03 11:14:42 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -189,7 +189,7 @@ function addRecent(data) {
       const itemLink = document.createElement("a");
       itemLink.href - "#";
       itemLink.target = "_blank";
-      itemLink.className = "temp-link-block w-inline block";
+      itemLink.className = "";
       itemLink.innerHTML = `<a href="#" class="temp-link-block w-inline-block"><div class="title-wrapper-listing"><div class="list-blue-title"><p class="bold-listing-title-mil">${row.County} <br><span title="Province" class="group-name-span">${row.Province}</span></p></div><div class="subtitle-wrapper"><div class="listing-subtitle-wrapper"><div title="Date" class="icon-featurelist w-embed"><svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M17 3H21C21.2652 3 21.5196 3.10536 21.7071 3.29289C21.8946 3.48043 22 3.73478 22 4V20C22 20.2652 21.8946 20.5196 21.7071 20.7071C21.5196 20.8946 21.2652 21 21 21H3C2.73478 21 2.48043 20.8946 2.29289 20.7071C2.10536 20.5196 2 20.2652 2 20V4C2 3.73478 2.10536 3.48043 2.29289 3.29289C2.48043 3.10536 2.73478 3 3 3H7V1H9V3H15V1H17V3ZM4 9V19H20V9H4ZM6 11H8V13H6V11ZM11 11H13V13H11V11ZM16 11H18V13H16V11Z" fill="currentColor"></path>
       </svg>
