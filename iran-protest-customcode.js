f7066425d2208fd45b989c6437062b61a5a96a91
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2023-01-05 19:38:12 +0530

    Update iran-protest-customcode.js

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -96,7 +96,7 @@ map.on("load", function () {
               "step",
               ["get", "point_count"],
               "#51bbd6",
-              5,
+              100,
               "#f1f075",
               750,
               "#f28cb1",
@@ -112,9 +112,48 @@ map.on("load", function () {
             ],
           },
         });
+        map.addLayer({
+          id: "cluster-count",
+          type: "symbol",
+          source: "protests",
+          filter: ["has", "point_count"],
+          layout: {
+            "text-field": ["get", "point_count_abbreviated"],
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+            "text-size": 12,
+          },
+        });
+        map.addLayer({
+          id: "unclustered-point",
+          type: "circle",
+          source: "protests",
+          filter: ["!", ["has", "point_count"]],
+          paint: {
+            "circle-color": "#11b4da",
+            "circle-radius": 4,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff",
+          },
+        });
+        map.on("click", "clusters", (e) => {
+          const features = map.queryRenderedFeatures(e.point, {
+            layers: ["clusters"],
+          });
+          const clusterId = features[0].properties.cluster_id;
+          map
+            .getSource("protests")
+            .getClusterExpansionZoom(clusterId, (err, zoom) => {
+              if (err) return;
+
+              map.easeTo({
+                center: features[0].geometry.coordinates,
+                zoom: zoom,
+              });
+            });
+        });
         // When a click event occurs on a feature in the csvData layer, open a popup at the
         // location of the feature, with description HTML from its properties.
-        map.on("click", "csvData", function (e) {
+        map.on("click", "unclustered-point", function (e) {
           var coordinates = e.features[0].geometry.coordinates.slice();
           //set popup text
           if (e.features[0].properties.videoid == "") {
@@ -169,6 +208,12 @@ map.on("load", function () {
           if (e.features[0].properties.Killed == "") {
             $(".popup-grid-div-killed").toggleClass("is-hidden");
           }
+        });
+        map.on("mouseenter", "clusters", () => {
+          map.getCanvas().style.cursor = "pointer";
+        });
+        map.on("mouseleave", "clusters", () => {
+          map.getCanvas().style.cursor = "";
         });
         map.on("mouseleave", "csvData", function () {
           map.getCanvas().style.cursor = "";
