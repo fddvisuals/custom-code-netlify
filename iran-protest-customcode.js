e35a4259e19ade250cc33e857bf4c8371f65412b
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2023-01-05 19:10:57 +0530

    init

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -74,48 +74,84 @@ map.on("load", function () {
       function (err, data) {
         // console.log(data);
         //Add the the layer to the map
-        map.addLayer({
-          id: "csvData",
+        map.addSource("protests", {
+          id: "clusters",
           type: "circle",
           source: {
             type: "geojson",
             data: data,
           },
+          filter: ["has", "point_count"],
           paint: {
-            "circle-radius": 5,
             "circle-color": [
-              "match",
-              ["get", "Estimated_Size"],
-              "Unspecified",
-              "hsl(357, 5%, 36%)",
-              "Medium",
-              "hsl(23, 89%, 45%)",
-              ["Small", "small"],
-              "hsl(49, 100%, 51%)",
-              "Large",
-              "hsl(0, 95%, 45%)",
-              "hsla(0, 0%, 0%, 0)",
+              "step",
+              ["get", "point_count"],
+              "#51bbd6",
+              100,
+              "#f1f075",
+              750,
+              "#f28cb1",
             ],
-            "circle-opacity": 0.5,
-            "circle-stroke-width": [
-              "case",
-              ["==", ["get", "Arrested"], 0],
-              0.75,
-              [">", ["get", "Arrested"], 10],
-              2,
-              [">", ["get", "Arrested"], 22],
-              4,
-              [">", ["get", "Arrested"], 880],
-              10,
-              0.75,
+            "circle-radius": [
+              "step",
+              ["get", "point_count"],
+              20,
+              100,
+              30,
+              750,
+              40,
             ],
-            "circle-stroke-color": "black",
-            "circle-radius": 8,
           },
         });
+        map.addLayer({
+          id: "cluster-count",
+          type: "symbol",
+          source: {
+            type: "geojson",
+            data: data,
+          },
+          filter: ["has", "point_count"],
+          layout: {
+            "text-field": ["get", "point_count_abbreviated"],
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+            "text-size": 12,
+          },
+        });
+        map.addLayer({
+          id: "unclustered-point",
+          type: "circle",
+          source: {
+            type: "geojson",
+            data: data,
+          },
+          filter: ["!", ["has", "point_count"]],
+          paint: {
+            "circle-color": "#11b4da",
+            "circle-radius": 4,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff",
+          },
+        });
+        map.on("click", "clusters", (e) => {
+          const features = map.queryRenderedFeatures(e.point, {
+            layers: ["clusters"],
+          });
+          const clusterId = features[0].properties.cluster_id;
+          map
+            .getSource("earthquakes")
+            .getClusterExpansionZoom(clusterId, (err, zoom) => {
+              if (err) return;
+
+              map.easeTo({
+                center: features[0].geometry.coordinates,
+                zoom: zoom,
+              });
+            });
+        });
+
         // When a click event occurs on a feature in the csvData layer, open a popup at the
         // location of the feature, with description HTML from its properties.
-        map.on("click", "csvData", function (e) {
+        map.on("click", "unclustered-point", function (e) {
           var coordinates = e.features[0].geometry.coordinates.slice();
           //set popup text
           if (e.features[0].properties.videoid == "") {
