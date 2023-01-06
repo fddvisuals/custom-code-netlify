10dea208c0d260eace200fd2aa68f9dfb97ebdff
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2023-01-06 13:53:28 +0530

    init

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -89,6 +89,9 @@ map.on("load", function () {
           clusterMaxZoom: 14,
           clusterRadius: 30,
         });
+        const popup = new mapboxgl.Popup({
+          closeButton: true,
+        });
         map.addLayer({
           id: "clusters",
           type: "circle",
@@ -266,7 +269,7 @@ map.on("load", function () {
 });
 const listingEl = document.getElementById("feature-listing");
 function addRecent(data) {
-  console.log(data);
+  // console.log(data);
   data
     .slice()
     .reverse()
@@ -282,6 +285,21 @@ function addRecent(data) {
       <path d="M1 21.9999C1 19.8782 1.84285 17.8434 3.34315 16.3431C4.84344 14.8428 6.87827 13.9999 9 13.9999C11.1217 13.9999 13.1566 14.8428 14.6569 16.3431C16.1571 17.8434 17 19.8782 17 21.9999H1ZM9 12.9999C5.685 12.9999 3 10.3149 3 6.99994C3 3.68494 5.685 0.999936 9 0.999936C12.315 0.999936 15 3.68494 15 6.99994C15 10.3149 12.315 12.9999 9 12.9999ZM18.246 3.18394C18.7454 4.39409 19.0016 5.69077 19 6.99994C19.0016 8.3091 18.7454 9.60578 18.246 10.8159L16.569 9.59593C16.8552 8.76037 17.0008 7.88314 17 6.99994C17.0011 6.11678 16.8558 5.23956 16.57 4.40394L18.246 3.18394V3.18394ZM21.548 0.783936C22.5062 2.71576 23.0032 4.84353 23 6.99994C23 9.23294 22.477 11.3439 21.548 13.2159L19.903 12.0199C20.6282 10.4459 21.0025 8.733 21 6.99994C21 5.20794 20.607 3.50694 19.903 1.97994L21.548 0.783936V0.783936Z" fill="currentColor"></path>
       </svg>
       </div><p class="bold-listing-subtitle">${row.Estimated_Size}</p></div></div></div><p class="listing-text">${row.Description}<br></p></a>`;
+      // itemLink.addEventListener("mouseover", () => {
+      //   popupGenerator(feature);
+      // });
+      itemLink.onclick = function () {
+        // setActive(listing);
+
+        // When a menu item is clicked, animate the map to center
+        // its associated locale and open its popup.
+        map.flyTo({
+          center: feature.geometry.coordinates,
+          zoom: 16,
+        });
+        //locale.openPopup();
+        return false;
+      };
       listingEl.appendChild(itemLink);
     });
 }
