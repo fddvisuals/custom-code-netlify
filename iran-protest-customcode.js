f77f380808b9d802daa9fa6ace74664d47087cdb
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2023-01-06 13:35:27 +0530

    a

--- iran-protest-customcode.js
+++ iran-protest-customcode.js
@@ -160,7 +160,7 @@ map.on("load", function () {
         document.getElementById("reset-view").addEventListener("click", () => {
           // Fly to a random location
           map.flyTo({
-            center: [40.44, 39.5],
+            center: [50.44, 29.5],
             zoom: 5,
             essential: true, // this animation is considered essential with respect to prefers-reduced-motion
           });
