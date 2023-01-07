e9ac61181a4c1105fa45537922ba8adcd60bfc27
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2023-01-07 12:49:26 +0530

    init

--- mapping-west-bank-terrorism.js
+++ mapping-west-bank-terrorism.js
@@ -411,3 +411,43 @@ function renderList() {
     wbevents = uniqueFeatures;
   }
 }
+
+Papa.parse(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSF2MCbKjaGWAqudInjQIsv5KV8SJHCGhpXyd5_ncpUQfUeTwboBZP_ALsa3ybJw95ro8Zq4a_A2btv/pub?gid=0&single=true&output=csv",
+  {
+    download: true,
+    header: true,
+    complete: function (csvData) {
+      addRecent(csvData.data);
+    },
+  }
+);
+const listingEl2 = document.getElementById("feature-listing-1");
+function addRecent(data) {
+  console.log(data);
+  data
+    .slice()
+    .reverse()
+    .forEach(function (row) {
+      const itemLink2 = document.createElement("a");
+      itemLink2.href - "#";
+      itemLink2.target = "_blank";
+      itemLink2.className = "";
+      itemLink2.innerHTML = `<div role="listitem" class="wb_related_item w-dyn-item"><a href="${row.Link}" target="_blank" class="related-pub-link-block w-inline-block"><h1 class="publications-title small">${row.Title}</h1><div class="related-subtitle-italics">${row.Author}</div><div class="subtitle-horizontal-wrapper"><div class="related-subtitle">${row.Date}</div><div class="related-subtitle">, &nbsp;</div><div class="related-subtitle">${row.Source}</div></div></a></div>`;
+      // itemLink.addEventListener("mouseover", () => {
+      //   popupGenerator(feature);
+      // });
+      itemLink2.onclick = function () {
+        // setActive(listing);
+        // When a menu item is clicked, animate the map to center
+        // its associated locale and open its popup.
+        map.flyTo({
+          center: feature.geometry.coordinates,
+          zoom: 16,
+        });
+        //locale.openPopup();
+        return false;
+      };
+      listingEl2.appendChild(itemLink2);
+    });
+}
